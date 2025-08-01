0ad826edab37a8572a9b20f061e403ceaa3a24b7
AugustZer0 <qweraz157890@gmail.com>
2025-08-02 07:14:16 +0900

    CORS

--- src/main.py
+++ src/main.py
@@ -5,9 +5,13 @@ app = FastAPI()
 
 from fastapi.middleware.cors import CORSMiddleware
 
+origins = [
+    "https://KNU-SingalProject.github.io"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 배포 시에는 특정 도메인만
+    allow_origins=[origins],  # 배포 시에는 특정 도메인만
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
